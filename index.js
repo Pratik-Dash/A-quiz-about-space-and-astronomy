c149517b9aa83a64b1df173280a95b5dac289d1d
Pratik Dash <>
2022-07-17 09:46:39 +0000

    Style - Added Dashes between each Question Answer Pair.

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
 import chalk from "chalk";
 import readlinesync from 'readline-sync'
+var playerScore = 0
+var playerName = ""
+var newOrExistingPlayer = 0
+var playerChoice = "Yes"
+//0 - New Player 1 - Existing player
+var highScores = [
+  {
+    "Name":"Asish",
+    "Score":3
+  },
+  {
+    "Name":"Subhasish",
+    "Score":4
+  }
+]
+function welcomePlayer(newOrExistingPlayer)
+  {
+    
+    console.log("Welcome Player!")
+    console.log("\n")
+    playerName = readlinesync.question("Please enter your name \n");
+    console.log("\n")
+    for(var i = 0;i<highScores.length;i++)
+      {
+        if(playerName.toLowerCase() == highScores[i].Name.toLowerCase())
+        {
+          newOrExistingPlayer = 1
+          break;
+         
+        
+      }
+        else{
+          newOrExistingPlayer = 0
+        }
+      }
+        if(newOrExistingPlayer == 1)
+        {
+           console.log("You already are an existing player. Your highest score is "+highScores[i].Score);
+          console.log("\n")
 
-var highScore = [3,4,];
+         var playerChoice = readlinesync.question("Please note that your score will only be updated if current score is higher than your previous high score. Would you like to continue? (Enter 'Y' or 'N') \n")
 
+          if(playerChoice === "Y" )
+          {
+            Play(playerName,newOrExistingPlayer)
+          }
+          else{
+            return 
+          }
+          
+        }
+        else{
+          console.log("You are a new player. Starting Quiz...")
+          Play(playerName,newOrExistingPlayer)
+        }
+        
+    
+    
+  }
 //List of Questions
-function Play(highscore){
-  
+function Play(playerName,newOrExistingPlayer){
+  var playerStatus = newOrExistingPlayer
+  var name  = playerName
   var score = 0;
   console.log("Starting quiz......\n")
   var questions = [
@@ -29,52 +86,100 @@ function Play(highscore){
     "question":"what is my favorite programming language?\n",
     "answer":"C#"
     }
-];
+]
   console.log(chalk.inverse('your current score is : '+score));
-
-   console.log("\n");
+console.log("------------------")
+   console.log("\n")
 var userAnswer;
 
   for(var i = 0;i<questions.length;i++)
     {
-      userAnswer = readlinesync.question(questions[i].question);
-      console.log("\n");
+      userAnswer = readlinesync.question(questions[i].question)
+      console.log("\n")
       
     if(userAnswer.toLowerCase() === questions[i].answer.toLowerCase())
       {
         
         score++;
-        console.log(chalk.bgGreen('Correct Answer !')+"\n"+chalk.inverse('Your Score is : '+score));
-        userAnswer = "";
+        console.log(chalk.bgGreen('Correct Answer !')+"\n"+chalk.inverse('Your Score is : '+score))
+        console.log("------------------")
+        console.log("\n")
       }
       else{
-        console.log(chalk.bgRed('Wrong Answer')+"\n"+ chalk.inverse('your score is : '+score));
+        console.log(chalk.bgRed('Wrong Answer')+"\n"+ chalk.inverse('your score is : '+score))
+        console.log("------------------")
+        console.log("\n")
       }
     }
-if(score>=4)
-{
-  console.log("Well done you have got  "+score+" questions correct")
+
+  CalculateHighScore(score,name,playerStatus)
 }
-else{
-  console.log("Better Luck next time ");
-}
-  for(var j = 0;j<highScore.length;j++)
+
+function CalculateHighScore(score,name,playerStatus)
+  {
+    if(playerStatus == 1)
     {
-      if(score > highScore[j])
+      for(var i = 0;i<highScores.length;i++)
       {
-        if(score>highScore[j+1])
+        if(playerName.toLowerCase() == highScores[i].Name.toLowerCase())
         {
-          highscore[highScore.length-1] = score;
-          console.log(chalk.bgYellowBright('Congratulatons! You have the highest score in the game\n'));
-console.log("Your final score is - "+score)
-        
+          if(score>highScores[i].Score)
+          {
+            console.log("Your previous score was : "+highScores[i].Score+" Your new Score is : "+score+" Congratulations your beat your own highscore")
+            highScores[i].Score = score;
+          }
         }
       }
+      console.log("\n")
+      console.log("Top Scorers \n")
+      
+      for(var i = 0;i<highScores.length;i++)
+        {
+          console.log(highScores[i].Name+" - "+highScores[i].Score)
+        }
     }
+    else{
+      var temp = false;
+      for(var i = 0;i<highScores.length;i++)
+        {
+          if(score>highScores[i].Score)
+          {
+            temp = true
+          }else{
+            temp == false
+          }
+          
+        }
+      if(temp == true)
+      {
+        console.log("New high score detected! \n")
+        console.log("Adding new high score...\n")
+        highScores.push({
+          "Name":playerName,
+          "Score":score
+          
+        })
+        
+      }
+      else{
+        console.log("your final score is "+score)
+      }
+     
 
-  
-}
+      console.log("Top Scorers")
+      console.log("------------------")
+      for(var i = 0;i<highScores.length;i++)
+        {
+          console.log(highScores[i].Name+" - "+highScores[i].Score);
+        }
+      
+    }
+    
+  }
 
-Play(highScore);
+
+  welcomePlayer(newOrExistingPlayer)
+
+
 
 
